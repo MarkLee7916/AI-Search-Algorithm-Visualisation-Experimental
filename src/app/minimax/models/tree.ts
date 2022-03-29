d3937db9b5e9aaf7bb61e9b2c0137071e6f83832
Mark Lee <cyb18134@uni.strath.ac.uk>
2022-03-29 12:17:39 +0100

    Finish comments for minimax module

--- src/app/minimax/models/tree.ts
+++ src/app/minimax/models/tree.ts
@@ -1,9 +1,12 @@
 import { assertDefined, sequenceBetween } from 'src/app/shared/genericUtils';
 
+// The depth of the tree
 export const TREE_DEPTH = 4;
 
+// The number of leaves of the tree
 export const LEAF_COUNT = Math.pow(2, TREE_DEPTH);
 
+// A node worked on by the internal minimax algorithm
 export type Node = {
   alpha: number | null;
   beta: number | null;
@@ -16,6 +19,7 @@ export type Node = {
   id: string;
 };
 
+// Wrapper to generate Node objects
 export function genDefaultNode(id: string, depth: number): Node {
   return {
     alpha: null,
@@ -30,22 +34,27 @@ export function genDefaultNode(id: string, depth: number): Node {
   };
 }
 
+// Convert a tree to an inorder list, preserving its structure, useful for testing
 export function flattenTreeToInorderList(node: Node): Node[] {
   return flattenTreeToInorderListRecurse(node, []);
 }
 
+// Build a full binary tree from a list of leaf values
 export function buildBinaryTree(leafValues: number[]): Node {
   return buildBinaryTreeRecurse(0, [...leafValues], '');
 }
 
+// Return true if a node has no children
 export function isLeafNode(node: Node): boolean {
   return node.leftChild === null && node.rightChild === null;
 }
 
+// Return true if a node has no parents
 export function isRootNode(node: Node): boolean {
   return node.depth === 0;
 }
 
+// Recursive implementation of flattenTreeToInorderList()
 function flattenTreeToInorderListRecurse(node: Node, list: Node[]): Node[] {
   if (node.leftChild !== null) {
     flattenTreeToInorderListRecurse(node.leftChild, list);
@@ -60,6 +69,7 @@ function flattenTreeToInorderListRecurse(node: Node, list: Node[]): Node[] {
   return list;
 }
 
+// Recursive implementation of buildBinaryTree()
 function buildBinaryTreeRecurse(
   depth: number,
   leafValues: number[],
