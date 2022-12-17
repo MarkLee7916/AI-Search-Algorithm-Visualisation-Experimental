8a37f78863d66fc07ba003fb74e6fa5a14481746
Mark Lee <cyb18134@uni.strath.ac.uk>
2022-12-17 09:03:31 +0000

    Switch mouse pressing to a service

---
+++ src/app/pathfinding/services/mouse-press.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { fromEvent } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MousePressService {
+  private isMouseDown = false;
+
+  constructor() {
+    fromEvent(document, 'mousedown').subscribe(this.handlePress.bind(this));
+    fromEvent(document, 'mouseup').subscribe(this.handlePress.bind(this));
+  }
+
+  public getMouseDown(): boolean {
+    return this.isMouseDown;
+  }
+
+  public toggleMouseDown(): void {
+    this.isMouseDown = !this.isMouseDown;
+  }
+
+  private handlePress(event: Event): void {
+    this.isMouseDown = (event as MouseEvent).buttons === 1;
+  }
+}
